60823507ccdee769a8c00d51b19474024e9c59a6
Jeffrey Lym <jeffreyalym@gmail.com>
2018-08-18 22:44:32 -0700

    Connect app to apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
+import ApolloClient from "apollo-boost";
 import * as React from 'react';
+import { ApolloProvider } from "react-apollo";
 import './App.css';
 import Grid from './Grid';
 
+const client = new ApolloClient();
+
 class App extends React.Component {
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Project Outline
-        </header>
-        <main className="main">
-          <Grid/>
-        </main>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            Project Outline
+          </header>
+          <main className="main">
+            <Grid/>
+          </main>
+        </div>
+      </ApolloProvider>
     );
   }
 }
